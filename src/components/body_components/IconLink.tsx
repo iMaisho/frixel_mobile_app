38cfe37f3a02a24aff68256c375e8c5f1d8f0180
Antonin M. <antonin@frixel.fr>
2025-10-01 16:44:47 +0200

    FM-14 feat(iconLinks)

---
+++ src/components/body_components/IconLink.tsx
@@ -0,0 +1,37 @@
+import { Image, Linking, StyleSheet, TouchableOpacity } from "react-native";
+
+const ICONS = {
+  "github": require("../../assets/images/github-icon.png"),
+  "linkedin": require("../../assets/images/linkedin-icon.png"),
+} as const;
+
+type IconName = keyof typeof ICONS;
+
+interface IconLinkProps {
+  url: string;
+  iconName: IconName;
+}
+
+function IconLink({ url, iconName }: IconLinkProps) {
+
+    const styles = StyleSheet.create({
+      container: {
+        width: 40,
+        height: 40,
+      },
+      icon: {
+        width: 40,
+        height: 40,
+        borderRadius: 50
+      }
+    })
+
+    return (
+      <TouchableOpacity style={styles.container} onPress={() => Linking.openURL(url)}>
+        <Image source={ICONS[iconName]} style={styles.icon} />
+      </TouchableOpacity>
+    )
+  }
+
+
+export default IconLink;
